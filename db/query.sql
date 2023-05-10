681facab49b1f8c1ccec9f7fcc5f899a12b9b05e
Giuseppe_Raddato <e-geto@gft.com>
2023-05-10 16:44:10 +0200

    aggiunta query.sql con la query per trovare i super eroi con i loro poteri

--- db/query.sql
+++ db/query.sql
@@ -4,11 +4,12 @@ select su.superhero_name,
        su.full_name,
        p.publisher_name,
        GROUP_CONCAT(DISTINCT  s.power_name
-                    ORDER BY  s.power_name DESC SEPARATOR ',')
+                    ORDER BY  s.power_name ASC SEPARATOR ',')
 from superhero su
          inner join publisher p on p.id=su.publisher_id
          inner join hero_power hp on su.id = hp.hero_id
          inner join superpower s on hp.power_id = s.id
-where  publisher_name = 'Marvel Comics'
-group by su.superhero_name, su.full_name, p.publisher_name;
+-- where  publisher_name = 'Marvel Comics'
+group by su.superhero_name, su.full_name, p.publisher_name
+order by  publisher_name,superhero_name;
 
