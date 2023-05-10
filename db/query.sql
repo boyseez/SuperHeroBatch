ced6547d6d635ffb1dd375ce8fc1720b6c0055d6
Giuseppe_Raddato <e-geto@gft.com>
2023-05-10 16:41:26 +0200

    aggiunta query.sql con la query per trovare i super eroi marvel

---
+++ db/query.sql
@@ -0,0 +1,14 @@
+-- trovare tutti i super eroi della marvel con i loro nomi originali
+-- e la lista di super poteri
+select su.superhero_name,
+       su.full_name,
+       p.publisher_name,
+       GROUP_CONCAT(DISTINCT  s.power_name
+                    ORDER BY  s.power_name DESC SEPARATOR ',')
+from superhero su
+         inner join publisher p on p.id=su.publisher_id
+         inner join hero_power hp on su.id = hp.hero_id
+         inner join superpower s on hp.power_id = s.id
+where  publisher_name = 'Marvel Comics'
+group by su.superhero_name, su.full_name, p.publisher_name;
+
