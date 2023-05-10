9df9586e17e0596e3842ac212e823f6bb3482c21
Giuseppe_Raddato <e-geto@gft.com>
2023-05-10 15:04:05 +0200

    Initial commit

---
+++ src/main/java/it/esempio/superherobatch/SuperHeroBatchApplication.java
@@ -0,0 +1,13 @@
+package it.esempio.superherobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperHeroBatchApplication.class, args);
+    }
+//
+}
---
+++ src/test/java/it/esempio/superherobatch/SuperHeroBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package it.esempio.superherobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
